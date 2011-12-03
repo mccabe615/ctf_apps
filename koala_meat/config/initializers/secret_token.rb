eb0b6b963520cc5d8af6a0d97ad3e97f6f84b88d
Ken Johnson <cktricky@gmail.com>
2011-12-03 17:23:51 -0500

    Bare bones, controller not generated, db not configured. Starting from zee scratches, wah wah wee wah

---
+++ koala_meat/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KoalaMeat::Application.config.secret_token = 'c3a5d821cb5b70e43696f8821fe33351026140044f720ccf34208ad20b827ed743f3dd9ba44d42c2dffe000b7fa5cea3ed64c6d9e20f4b9a30e091aa2d0138da'
