cfb36ae8515372e08ecb2eeefafdc2d2daebc38a
cktricky <enduser@Livings-MacBook-Pro-148.local>
2012-03-10 16:18:41 -0500

    added some mass assignment fail

--- koala_meat/app/models/user.rb
+++ koala_meat/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :password, :password_confirmation
+  #attr_accessible :email, :password, :password_confirmation, :admin
   
   attr_accessor :password
   before_save :encrypt_password
