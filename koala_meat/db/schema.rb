c4589afd5e30dba57fc08d9427457bed03b4e5bd
Ken Johnson <cktricky@gmail.com>
2012-03-16 23:16:59 -0400

    fixed the schema again

--- koala_meat/db/schema.rb
+++ koala_meat/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120310154432) do
+ActiveRecord::Schema.define(:version => 20120311092815) do
 
   create_table "users", :force => true do |t|
     t.string   "email"
