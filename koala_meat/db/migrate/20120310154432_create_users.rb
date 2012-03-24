77debe08df26c15b2e01bae09628cd69d1542da6
Ken Johnson <cktricky@gmail.com>
2012-03-24 13:07:20 -0400

    changed the migration file so the admin column will exist

--- koala_meat/db/migrate/20120310154432_create_users.rb
+++ koala_meat/db/migrate/20120310154432_create_users.rb
@@ -2,6 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :email
+      t.boolean :admin
       t.string :password_hash
       t.string :password_salt
 
