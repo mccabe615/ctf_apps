cec78aab444f101a011f8c2058cdb4c6a4f30241
Ken Johnson <cktricky@gmail.com>
2012-04-04 07:59:05 -0400

    added a hint and some other stuff

--- collabit/app/controllers/users_controller.rb
+++ collabit/app/controllers/users_controller.rb
@@ -6,11 +6,25 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(params[:user])
+    @user.user_id = @user.id if (@user.respond_to?('id')) && (@user.respond_to?('user_id'))
     if @user.save
       redirect_to root_url, :notice => "Signed up!"
     else
       render "new"
     end
+  end
+  
+  def test
+    render :text => %q{
+    
+   <p> Nice try! Hey, here is a game related hint for your troubles:</p>
+    
+   <p> "Know your roles" </p>
+    
+    <p>and ...</p>
+    
+    <p>a = (0..4), a.each_with_idx do {|i, ix| "w00t" if ix == a.first}</p>
+    }
   end
 
 end
--- collabit/config/routes.rb
+++ collabit/config/routes.rb
@@ -1,9 +1,8 @@
 Collabit::Application.routes.draw do
-  get "sessions/new"
-  get "users/new"
   get "logout" => "sessions#destroy", :as => "logout"
   get "login" => "sessions#new", :as => "log_in"
   get "register" => "users#new", :as => "register"
+  get "test" => "users#test", :as => "test"
   get "home" => "home#index", :as => "home"
   get "latest" => "status#latest_updates",:as => "latest"
   get "create" => "status#create", :as => "create"
