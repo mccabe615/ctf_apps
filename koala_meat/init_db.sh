7bbcd0065920bb5ea7779fc334beaf20ad5988df
Ken Johnson <cktricky@gmail.com>
2012-07-29 11:56:07 -0400

    cleaned up the db initialize script

--- koala_meat/init_db.sh
+++ koala_meat/init_db.sh
@@ -1,10 +1,11 @@
-# Development
+# Drop existing db
+rvmsudo bundle exec rake db:drop
+
+# Create a fresh db
 rvmsudo bundle exec rake db:create
+
+# Load the db schema
 rvmsudo bundle exec rake db:schema:load
+
+# Put data into the db from the db/seeds.rb file
 rvmsudo bundle exec rake db:seed
-
-#Production
-
-rvmsudo RAILS_ENV=production bundle exec rake db:create
-rvmsudo RAILS_ENV=production bundle exec rake db:schema:load
-rvmsudo RAILS_ENV=production bundle exec rake db:seed
